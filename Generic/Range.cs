ba27655c5a5d6e6ec7a7d615de4837b63861778f
Mohamed hamza <MohHamza106>
2025-01-29 16:05:19 +0200

    Question 1

---
+++ Generic/Range.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_c_.Generic
+{
+    internal class Range<T> where T:IComparable<T>, INumber<T>
+    {
+        public T minimum { get; set; }
+        public T maximum { get; set; }
+        public Range(T minimum, T maximum)
+        {
+            this.minimum = minimum;
+            this.maximum = maximum;
+        }
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo (minimum) >= 0 && value.CompareTo(maximum) <= 0;
+        }
+        public T Length()
+        {
+            return maximum -minimum; 
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Generics_c_
+﻿using Generics_c_.Generic;
+
+namespace Generics_c_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //var Range = new Range<int>(50, 80);
+            //Console.WriteLine(Range.IsInRange(15)); 
+            //Console.WriteLine(Range.IsInRange(70));
+            //Console.WriteLine(Range.Length());
+            #endregion
+
         }
     }
 }
